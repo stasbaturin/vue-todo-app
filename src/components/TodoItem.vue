c166162d6790617c06350fd5b6551dcea3e985b7
sbaturin <sbaturin@vm-ub2204-srv1.sbat.pro>
2023-03-11 19:24:09 +0300

    fix checkbox value reset after applying filter

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,8 +1,7 @@
 <template>
     <li>
         <span v-bind:class="{done: todo.completed}">
-            <input type="checkbox"
-                v-on:change="todo.completed = !todo.completed">
+            <input type="checkbox" v-model="todo.completed">
             <strong>{{index + 1}}</strong>
             {{todo.title | uppercase}}
         </span>
